c1164549c27ff9c113be023c0a25c0b04b1e8423
SERajamani <rajamanise21@gmail.com>
2024-09-23 19:51:47 +0530

    learning es6

---
+++ funtion.js
@@ -0,0 +1,38 @@
+var phone = function(){
+    console.log("hiii")
+}
+
+phone()
+
+var ph = ()=>{
+    console.log("hello")
+}
+
+ph()
+
+// Task
+
+function add(a,b)
+{
+    console.log(a+b)
+
+}
+add(10,20)
+
+// change this to arrow function
+
+let addition=(a,b)=>{
+    console.log(a+b)
+}
+addition(30,40)
+
+// `we can write in many lines.. it does not show any error`
+
+// template literals- ${}
+var name = "raja"
+console.log(`My name is :${name}`)
+
+
+
+
+
